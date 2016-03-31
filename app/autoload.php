e43db14eda2428c9394fe5d4c5ff7183bc697e44
David Buchmann <david@liip.ch>
2016-03-31 16:05:21 -0400

    Applied fixes from StyleCI

--- app/autoload.php
+++ app/autoload.php
@@ -1,10 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /**
- * @var ClassLoader $loader
+ * @var ClassLoader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
--- web/app.php
+++ web/app.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Symfony\Component\HttpFoundation\Request;
 
-/**
+/*
  * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
